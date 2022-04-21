83d2a7d1a85d641ad06d02c899417a50c1caaaf8
choijueun <songjueun815@gmail.com>
2022-04-21 18:52:45 +0900

    JSX

--- blog/src/App.js
+++ blog/src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            
+            <div className="black-nav">
+                react에서는 html 대신 jsx 사용
+            </div>
         </div>
     );
 }
