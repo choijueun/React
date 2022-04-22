11cdee4f7ea5d615f57504260e52e40bd9e3550d
choijueun <songjueun815@gmail.com>
2022-04-22 18:46:15 +0900

    JSX: style

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,20 +1,14 @@
 import './App.css';
-import logo from './logo.svg'
 
 function App() {
     let posts = '양재동 맛집';
-    function Fn() {
-        return 100;
-    }
 
     return (
         <div className="App">
             <div className="black-nav">
-                <div>개발 Blog</div>
+                <div style={ {color:'blue', fontSize: '30px'} }>개발 Blog</div>
             </div>
             <h4>{ posts }</h4>
-            <h3>{ Fn() }</h3>
-            <img src={logo}/>
         </div>
     );
 }
