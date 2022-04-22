f856a81b79f3d72252d2c448ed8e0cb761b05003
choijueun <songjueun815@gmail.com>
2022-04-22 18:38:05 +0900

    Data Binding

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 
 function App() {
+    let posts = '양재동 맛집';
+
     return (
         <div className="App">
             <div className="black-nav">
                 <div>개발 Blog</div>
             </div>
+            <h4>{ posts }</h4>
         </div>
     );
 }
