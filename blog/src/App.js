3dcf4acf1ab51e3234f022386a332ea0b2c319f7
CJE <songjueun815@gmail.com>
2022-04-24 00:58:23 +0900

    state data array modify

--- blog/src/App.js
+++ blog/src/App.js
@@ -5,19 +5,31 @@ function App() {
     // let posts = '양재동 맛집';
     let [title, mTitle] = useState( ['하데스타운', '레베카', '라흐마니노프'] );
     let [recm, mRecm] = useState(0);
-
+    function changeTitle() {
+        // 복사(deep copy) spread operator
+        var tmpArr = [...title]; // ... : 중괄호 제거
+        // 복사본 수정
+        tmpArr[0] = '노트르담 드 파리';
+        // state data 변경
+        mTitle(tmpArr);
+    }
     return (
         <div className="App">
             <div className="black-nav">
                 <div style={ {color:'blue', fontSize: '30px'} }>개발 Blog</div>
             </div>
 
+            {/* 
             <button className="changeBtn" onClick={
                 ()=>{
                     let tmpTitle = ['노트르담 드 파리', {title}[1], {title}[2]];
                     mTitle(tmpTitle);
                 }
-            }>버튼</button>
+            }>버튼</button> 
+            */}
+
+            {/* changeTitle() : 바로 실행되므로 함수명만 작성 */}
+            <button onClick={ changeTitle }>제목바꾸기</button>
 
             <div className="list">
                 <h3>{ title[0] }</h3>
