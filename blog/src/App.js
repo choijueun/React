6e0268ceb6257df2c845e9fcf7e96ec8763a0f45
CJE <songjueun815@gmail.com>
2022-04-24 20:24:27 +0900

    Component

--- blog/src/App.js
+++ blog/src/App.js
@@ -49,14 +49,20 @@ function App() {
                 <hr/>
             </div>
 
-
-            <div className="modal">
-                <h2>제목</h2>
-                <p>날짜</p>
-                <p>상세내용</p>
-            </div>
+            <Modal></Modal>
         </div>
     );
+}
+
+// Component
+function Modal(){
+    return(
+        <div className="modal">
+            <h2>제목</h2>
+            <p>날짜</p>
+            <p>상세내용</p>
+        </div>
+    )
 }
 
 export default App;
