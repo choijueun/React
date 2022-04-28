139ecf7c71de1c9d12467da72a94214a7c7cdfbb
choijueun <songjueun815@gmail.com>
2022-04-28 18:42:10 +0900

    src/components/views의 page 넣어줄거라 뺌

--- blog/src/App.js
+++ blog/src/App.js
@@ -24,28 +24,4 @@ function App() {
     );
 }
 
-function Home() {
-    return(
-        <div>
-            <h2>Home</h2>
-        </div>
-    );
-}
-
-function About() {
-    return(
-        <div>
-            <h2>About</h2>
-        </div>
-    );
-}
-
-function Users() {
-    return(
-        <div>
-            <h2>Users</h2>
-        </div>
-    );
-}
-
 export default App;
