6c320683bc19b69fd4ac18c54f8865df82ae7ce8
choijueun <songjueun815@gmail.com>
2022-05-02 18:38:12 +0900

    axios

--- blog/src/components/views/LandingPage/LandingPage.js
+++ blog/src/components/views/LandingPage/LandingPage.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
 
 function LandingPage() {
+    // Hook
+    useEffect(()=>{
+        axios.get('/api/hello')
+        .then(response => console.log(response.data))
+    }, [])
+
     return (
         <div>
             LandingPage
