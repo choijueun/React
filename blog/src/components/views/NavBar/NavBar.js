d57b8877f598cb389d6c8a6dde9533f9824a151c
choijueun <songjueun815@gmail.com>
2022-05-09 23:27:18 +0900

    <li> 목록 만듦

--- blog/src/components/views/NavBar/NavBar.js
+++ blog/src/components/views/NavBar/NavBar.js
@@ -1,6 +1,11 @@
 import React from 'react'
 
 function NavBar(props) {
+    const lis = []
+    for(let i=0; i<props.topics.length; i++){
+        let tmp = props.topics[i];
+        lis.push(<li>{tmp.title}</li>)
+    }
 
     return (
         <div>
