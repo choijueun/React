94247dff9a983f31d0bd5a49b1084af59838b9c8
choijueun <songjueun815@gmail.com>
2022-05-09 23:29:13 +0900

    unique key

--- blog/src/components/views/NavBar/NavBar.js
+++ blog/src/components/views/NavBar/NavBar.js
@@ -4,7 +4,7 @@ function NavBar(props) {
     const lis = []
     for(let i=0; i<props.topics.length; i++){
         let tmp = props.topics[i];
-        lis.push(<li><a href={'/read/'+tmp.id}>{tmp.title}</a></li>)
+        lis.push(<li key={tmp.id}><a href={'/read/'+tmp.id}>{tmp.title}</a></li>)
     }
 
     return (
