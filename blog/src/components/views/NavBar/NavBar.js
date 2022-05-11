404a0ff5ea8a3618a5281d6f2338c50b3da7c5a3
choijueun <songjueun815@gmail.com>
2022-05-11 14:32:57 +0900

    nav event: arrow function, id

--- blog/src/components/views/NavBar/NavBar.js
+++ blog/src/components/views/NavBar/NavBar.js
@@ -4,7 +4,10 @@ function NavBar(props) {
     const lis = []
     for(let i=0; i<props.topics.length; i++){
         let tmp = props.topics[i];
-        lis.push(<li key={tmp.id}><a href={'/read/'+tmp.id}>{tmp.title}</a></li>)
+        lis.push(<li key={tmp.id}><a id={tmp.id} href={'/read/'+tmp.id} onClick={event=>{
+            event.preventDefault();
+            props.onChangeMode(event.target.id);
+        }}>{tmp.title}</a></li>)
     }
 
     return (
