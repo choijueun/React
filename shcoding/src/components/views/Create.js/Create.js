a539365eed27d64266d6a4a9b37036288f19e548
choijueun <songjueun815@gmail.com>
2022-05-11 16:20:46 +0900

    create component

---
+++ shcoding/src/components/views/Create.js/Create.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Create() {
+  return (
+    <div id="create">
+        <input type="text" placeholder='TITLE'></input>
+        <textarea placeholder='CONTENT'></textarea>
+    </div>
+  )
+}
+
+export default Create
