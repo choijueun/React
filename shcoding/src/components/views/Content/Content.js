cc05bbb7b16e6580ff979753d3803c8d91c126cf
choijueun <songjueun815@gmail.com>
2022-05-11 16:01:27 +0900

    동적 Content

--- shcoding/src/components/views/Content/Content.js
+++ shcoding/src/components/views/Content/Content.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
 function Content(props) {
-  return (
-    <div>{props.id}</div>
-  )
+    const topics = props.topics;
+
+    let title = '';
+    let content = '';
+    for (let i=0; i<topics.length; i++){
+        if (topics[i].id == props.id) {
+            title = <h1>{topics[i].title}</h1>;
+            content = <div>{topics[i].body}</div>
+        }
+    }
+
+    return (
+        <div>
+            {title}
+            {content}
+        </div>
+    )
 }
 
 export default Content
