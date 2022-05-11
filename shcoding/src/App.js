8ef37f1ac4cdc920bbce270ec9b0bc6440bc97e4
choijueun <songjueun815@gmail.com>
2022-05-11 15:42:45 +0900

    set topics

--- shcoding/src/App.js
+++ shcoding/src/App.js
@@ -4,6 +4,12 @@ import NavBar from './components/views/NavBar/NavBar'
 import Content from './components/views/Content/Content'
 
 function App() {
+    const topics = [
+        {id:1, title:'HTML', body:'html is ...'}
+        ,{id:2, title:'CSS', body:'css is ...'}
+        ,{id:3, title:'JavaScript', body:'javascript is ...'}
+    ]
+
     return (
         <>
             <Header></Header>
