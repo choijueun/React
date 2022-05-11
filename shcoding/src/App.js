a8b66447c94c7a1fbf96a06331320375429cf7d1
choijueun <songjueun815@gmail.com>
2022-05-11 15:39:10 +0900

    Components

--- shcoding/src/App.js
+++ shcoding/src/App.js
@@ -1,9 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/views/Header/Header'
+import NavBar from './components/views/NavBar/NavBar'
+import Content from './components/views/Content/Content'
 
 function App() {
     return (
         <>
+            <Header></Header>
+            <NavBar></NavBar>
+            <Content></Content>
         </>
     );
 }
