f2d0123ff78ecf45d8b11be86f3bedbd9eb30bd1
choijueun <songjueun815@gmail.com>
2022-05-13 18:04:39 +0900

    CREATE COPY

--- shcoding/src/components/views/Update/Update.js
+++ shcoding/src/components/views/Update/Update.js
@@ -1,9 +1,27 @@
 import React from 'react'
 
-function Update() {
-  return (
-    <div>Update</div>
-  )
+function Update(props) {
+    const updateSubmit = function(){
+        const updateForm = document.querySelector('#updateForm');
+        const title = updateForm.title.value;
+        const content = updateForm.content.value;
+    }
+    return (
+        <>
+        <h2>UPDATE</h2>
+        <form id="updateForm">
+            <div>
+                <input type="text" name="title" placeholder='TITLE'></input>
+            </div>
+            <div>
+                <textarea name="content" placeholder='CONTENT'></textarea>
+            </div>
+            <button type="button" className="submitBtn"onClick={updateSubmit}>
+                SUBMIT
+            </button>
+        </form>
+        </>
+    )
 }
 
 export default Update
