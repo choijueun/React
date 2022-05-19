b4199609bb34ac5767a8cfac5346cd10b1a75ebb
choijueun <songjueun815@gmail.com>
2022-05-19 21:26:58 +0900

    header

--- shcoding2/src/Component/views/Header/Header.js
+++ shcoding2/src/Component/views/Header/Header.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 class Header extends Component {
     render() {
         return (
-            <div>Header</div>
+            <header>
+                <h3>생활코딩!</h3>
+                <h1>React Programming</h1>
+            </header>
         )
     }
 }
