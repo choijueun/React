59b046c3ff4418c015371df459b3c8183525fd8d
choijueun <songjueun815@gmail.com>
2022-05-19 21:49:21 +0900

    주석

--- shcoding2/src/App.js
+++ shcoding2/src/App.js
@@ -5,6 +5,7 @@ import Header from './Component/views/Header/Header'
 import Nav from './Component/views/Nav/Nav'
 
 class App extends Component {
+    // function render() : function생략
     render () {
         return (
             <div className="App">
