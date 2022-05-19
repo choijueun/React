79db13b4309c78c685a4a8ed3c9a9deac46a359a
choijueun <songjueun815@gmail.com>
2022-05-19 21:23:59 +0900

    import components

--- shcoding2/src/App.js
+++ shcoding2/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Header from './Component/views/Header/Header'
+import Nav from './Component/views/Nav/Nav'
+
 class App extends Component() {
     render () {
         return (
             <div className="App">
-    
+                <Header/>
+                <Nav/>
             </div>
         );
     }
