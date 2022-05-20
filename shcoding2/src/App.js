941f61dd82c142963fd0267199911879c0fbf148
choijueun <songjueun815@gmail.com>
2022-05-20 17:06:49 +0900

    constructor

--- shcoding2/src/App.js
+++ shcoding2/src/App.js
@@ -6,6 +6,13 @@ import Nav from './components/views/Nav/Nav'
 import Content from './components/views/Content/Content'
 
 class App extends Component {
+    // 컴포넌트가 실행될 때 constructor가 가장 먼저 실행되어 초기화를 담당
+    constructor(props) {
+        super(props);
+        this.state = {
+            subject: {title:'WEB', content: 'World Wide Web!'}
+        }
+    }
     render () {
         return (
             <div className="App">
