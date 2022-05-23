a0f0308e1364c40cbea9723ed6fab28218653285
choijueun <songjueun815@gmail.com>
2022-05-23 18:47:26 +0900

    add Buttons

--- shcoding2/src/components/views/Content/Content.js
+++ shcoding2/src/components/views/Content/Content.js
@@ -6,11 +6,25 @@ class Content extends Component {
         const title = this.props.title;
         const desc = this.props.desc;
 
+        let btn = null;
+        if(this.props.mode === 'MAIN') {
+            btn = <div>
+                <button>CREATE</button>
+            </div>
+        }else if(this.props.mode === 'READ'){
+            btn = <div>
+                <button>CREATE</button>
+                <button>UPDATE</button>
+                <button>DELETE</button>
+            </div>
+        }
+
 
         return (
             <article>
                 <h3>{title}</h3>
                 {desc}
+                {btn}
             </article>
         )
     }
