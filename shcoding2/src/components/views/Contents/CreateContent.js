acfd98f85b308b821ecd4603dca4e814539334ba
choijueun <songjueun815@gmail.com>
2022-05-24 18:23:51 +0900

    create form

--- shcoding2/src/components/views/Contents/CreateContent.js
+++ shcoding2/src/components/views/Contents/CreateContent.js
@@ -3,15 +3,18 @@ import React from 'react'
 function CreateContent() {
   return (
     <article>
-        <div>
-            <input type='text' name="title" placeholder='TITLE'></input>
-        </div>
-        <div>
-            <textarea name="content" placeholder='CONTENT'></textarea>
-        </div>
-        <div className='submit'>
-            <button>SUBMIT</button>
-        </div>
+        <h2>CREATE</h2>
+        <form>
+            <div>
+                <input type='text' name="title" placeholder='TITLE'></input>
+            </div>
+            <div>
+                <textarea name="content" placeholder='CONTENT'></textarea>
+            </div>
+            <div className='submit'>
+                <button>SUBMIT</button>
+            </div>
+        </form>
     </article>
   )
 }
