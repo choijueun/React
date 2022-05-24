4764bad19ebf8488eed3f81d2607da1463093453
choijueun <songjueun815@gmail.com>
2022-05-24 18:20:40 +0900

    CreateContent

--- shcoding2/src/components/views/Control/Control.js
+++ shcoding2/src/components/views/Control/Control.js
@@ -3,7 +3,6 @@ import React from 'react'
 function Control(props) {
     let buttons = null;
     let chMode = props.changeHelper.chMode;
-    let chCont = props.changeHelper.chCont;
     let delCont = props.changeHelper.delCont;
 
     if(props.mode === 'MAIN') {
