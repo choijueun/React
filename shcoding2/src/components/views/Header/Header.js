d942e3f48d7ce5394ecc814c1e9fc50ab38a0be4
choijueun <songjueun815@gmail.com>
2022-05-25 21:11:33 +0900

    merge

--- shcoding2/src/components/views/Header/Header.js
+++ shcoding2/src/components/views/Header/Header.js
@@ -6,14 +6,10 @@ class Header extends Component {
         return (
             <header>
                 <h3>생활코딩!</h3>
-<<<<<<< HEAD:shcoding2/src/Component/views/Header/Header.js
-                <h1>{this.props.title}</h1>
-=======
                 <h1><a href="/" onClick={(e)=>{
                     e.preventDefault();
                     this.props.changeHelper.chMode('MAIN');
                 }}>{this.props.title}</a></h1>
->>>>>>> b393893f585dcdc953f003e450f50752bdc8d4ba:shcoding2/src/components/views/Header/Header.js
             </header>
         )
     }
