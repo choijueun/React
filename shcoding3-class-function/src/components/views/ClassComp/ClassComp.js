0c4ba498590b85a0f4ae22d9d661c02292152523
choijueun <songjueun815@gmail.com>
2022-05-29 13:28:04 +0900

    class style

--- shcoding3-class-function/src/components/views/ClassComp/ClassComp.js
+++ shcoding3-class-function/src/components/views/ClassComp/ClassComp.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
-function ClassComp() {
-  return (
-    <div className='container'>
-        <h2>Class Style Component</h2>
-    </div>
-  )
+class ClassComp extends React.Component {
+    render(){
+        return (
+          <div className='container'>
+              <h2>Class Style Component</h2>
+          </div>
+        )
+    }
 }
 
 export default ClassComp
