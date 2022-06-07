545d78a24981acb1d0288f0ca3bd83391b0f4432
choijueun <songjueun815@gmail.com>
2022-06-07 18:55:35 +0900

    useNavigate than <Redirect>

--- shcoding4-routerdom/src/components/views/TOC/TOC.js
+++ shcoding4-routerdom/src/components/views/TOC/TOC.js
@@ -1,13 +1,21 @@
 import React from 'react'
-import {NavLink} from 'react-router-dom'
+import {NavLink, useNavigate} from 'react-router-dom'
 
 function TOC() {
+    const navigate = useNavigate()
+    const redirect_guest = function(event){
+        event.preventDefault();
+        console.log('navigate()')
+        navigate('/guest', { replace: true });
+    }
+
     return (
         <nav>
             <ul>
                 <li><NavLink to="/">HOME</NavLink></li>
                 <li><NavLink to="/blog">BLOG</NavLink></li>
                 <li><NavLink to="/guest">GUEST</NavLink></li>
+                <li><NavLink to="/go" onClick={redirect_guest}>REDIRECT</NavLink></li>
             </ul>
         </nav>
     )
