0819be13cac5052c398e2e987eac674bf063a069
choijueun <songjueun815@gmail.com>
2022-06-13 17:32:15 +0900

    nav

--- shcoding5-ajax/src/App.js
+++ shcoding5-ajax/src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 
+function Nav() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="/">MAIN</a></li>
+                <li><a href="/1">HTML</a></li>
+                <li><a href="/2">CSS</a></li>
+                <li><a href="/3">JS</a></li>
+            </ul>
+        </nav>
+    )
+}
+
 function App() {
     return (
         <div className="App">
             <h1>WEB</h1>
-            <nav>
-
-            </nav>
+            <Nav/>
             <article>
 
             </article>
