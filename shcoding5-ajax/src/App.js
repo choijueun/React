befd225b4e58d89e00a51b06022a303243f81871
choijueun <songjueun815@gmail.com>
2022-06-13 17:55:25 +0900

    set li tags by fetchAPI

--- shcoding5-ajax/src/App.js
+++ shcoding5-ajax/src/App.js
@@ -1,23 +1,36 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function Nav() {
-    fetch('list.json')
-        .then(function(result){
-            // result = fetch된 파일의 내용
-            return result.json();
-        }).then(function(result){
-            // result = 첫 번째 then 함수의 반환값
-            console.log(result);
-        })
+    // set list object state
+    const [listObj, setListObj] = useState([])
+
+    // useEffect의 두 번째 인수로 빈 함수
+    // --> class 형식의 componentDidMount()처럼 한 번만 실행됨
+    useEffect(()=>{
+        //fetchAPI
+        fetch('list.json')
+            .then(function(result){
+                // result = fetch된 파일의 내용
+                return result.json();
+            }).then(function(result){
+                // result = 첫 번째 then 함수의 반환값
+                console.log(result);
+                setListObj(result);
+            })
+    }, [])
+
+    // li 태그
+    const liTag = [];
+    for(let i=0; i<listObj.length; i++) {
+        let li = listObj[i]
+        liTag.push(<li key={li.id}><a href={li.id}>{li.title}</a></li>)
+    }
     
     return (
         <nav>
             <ul>
-                <li><a href="/">MAIN</a></li>
-                <li><a href="/1">HTML</a></li>
-                <li><a href="/2">CSS</a></li>
-                <li><a href="/3">JS</a></li>
+                {liTag}
             </ul>
         </nav>
     )
