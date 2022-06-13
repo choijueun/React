957d5056256b63d13d38a1f17ff41b76c1f7dbb8
choijueun <songjueun815@gmail.com>
2022-06-13 18:15:26 +0900

    decoupling & refactoring

--- shcoding5-ajax/src/App.js
+++ shcoding5-ajax/src/App.js
@@ -2,35 +2,20 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function Nav(props) {
-    // set list object state
-    const [listObj, setListObj] = useState([])
-
-    // useEffect의 두 번째 인수로 빈 함수
-    // --> class 형식의 componentDidMount()처럼 한 번만 실행됨
-    useEffect(()=>{
-        //fetchAPI
-        fetch('list.json')
-            .then(function(result){
-                // result = fetch된 파일의 내용
-                return result.json();
-            }).then(function(result){
-                // result = 첫 번째 then 함수의 반환값
-                console.log(result);
-                setListObj(result);
-            })
-    }, [])
+    // onClick
+    const changePage = function(e){
+        e.preventDefault();
+        console.log('trigger');
+        props.onClick(e.target.dataset.id);
+    }
 
     // li 태그 배열
     const liTag = [];
-    for(let i=0; i<listObj.length; i++) {
-        let li = listObj[i]
+    for(let i=0; i<props.listObj.length; i++) {
+        let li = props.listObj[i]
         liTag.push(
         <li key={li.id}>
-            <a href={li.id} data-id={li.id} onClick={e=>{
-                e.preventDefault();
-                console.log('trigger');
-                props.onClick(e.target.dataset.id);
-            }}>
+            <a href={li.id} data-id={li.id} onClick={changePage}>
                 {li.title}
             </a>
         </li>
@@ -56,6 +41,24 @@ function Article(props) {
 }
 
 function App() {
+    // set list object state
+    const [listObj, setListObj] = useState([]);
+
+    // useEffect의 두 번째 인수로 빈 함수
+    // --> class 형식의 componentDidMount()처럼 한 번만 실행됨
+    useEffect(()=>{
+        //fetchAPI
+        fetch('list.json')
+            .then(function(result){
+                // result = fetch된 파일의 내용
+                return result.json();
+            }).then(function(result){
+                // result = 첫 번째 then 함수의 반환값
+                console.log(result);
+                setListObj(result);
+            })
+    }, [])
+
     const [content, setContent] = useState({'title': 'HELLO', 'desc': 'WORLD'});
     const changeContent = function(id) {
         fetch('content_'+id+'.json')
@@ -70,7 +73,8 @@ function App() {
     return (
         <div className="App">
             <h1>WEB</h1>
-            <Nav onClick={changeContent}/>
+            {/* 보여주는 기능만 담당하는 presentational component */}
+            <Nav listObj={listObj} onClick={changeContent}/>
             <Article title={content.title} desc={content.desc}/>
         </div>
     );
