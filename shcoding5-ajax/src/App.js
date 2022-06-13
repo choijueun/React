6f493902ab175864134b322e5757930eb8ccba7a
choijueun <songjueun815@gmail.com>
2022-06-13 17:07:19 +0900

    AJAX

---
+++ shcoding5-ajax/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>WEB</h1>
+            <nav>
+
+            </nav>
+            <article>
+                
+            </article>
+        </div>
+    );
+}
+
+export default App;
