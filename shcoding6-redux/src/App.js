8d49a2d988060ec97d74a756427654b0fa6f00bc
choijueun <songjueun815@gmail.com>
2022-06-15 16:34:11 +0900

    DisplayNumberRoot

--- shcoding6-redux/src/App.js
+++ shcoding6-redux/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import AddNumberRoot from './components/AddNumberRoot';
-import DisplayNumberRoot from './components/AddNumberRoot';
+import DisplayNumberRoot from './components/DisplayNumberRoot';
 
 
 function App() {
