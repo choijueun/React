dac898e11745f3046e0ca493709a9561127f74e2
choijueun <songjueun815@gmail.com>
2022-06-15 16:26:31 +0900

    split components

--- shcoding6-redux/src/App.js
+++ shcoding6-redux/src/App.js
@@ -1,41 +1,7 @@
 import './App.css';
+import AddNumberRoot from './components/AddNumberRoot';
+import DisplayNumberRoot from './components/AddNumberRoot';
 
-function AddNumber() {
-    return (
-        <div>
-            <h1>Add Number</h1>
-            <input type="button" value="+"></input>
-            <input type="text" defaultValue="0"></input>
-        </div>
-    )
-}
-
-function AddNumberRoot() {
-    return (
-        <div>
-            <h1>Add Number Root</h1>
-            <AddNumber/>
-        </div>
-    )
-}
-
-function DisplayNumber() {
-    return (
-        <div>
-            <h1>Display Number</h1>
-            <input type="text" defaultValue="0" readOnly></input>
-        </div>
-    )
-}
-
-function DisplayNumberRoot() {
-    return (
-        <div>
-            <h1>Display Number Root</h1>
-            <DisplayNumber/>
-        </div>
-    )
-}
 
 function App() {
     return (
---
+++ shcoding6-redux/src/components/DisplayNumber.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function DisplayNumber() {
+    return (
+        <div>
+            <h1>Display Number</h1>
+            <input type="text" defaultValue="0" readOnly></input>
+        </div>
+    )
+}
+
+export default DisplayNumber
