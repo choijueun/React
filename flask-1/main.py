d6dab1eb129567148cf5666904d8c823593e6bc0
choijueun <songjueun815@gmail.com>
2022-06-21 18:35:44 +0900

    change

--- flask-1/main.py
+++ flask-1/main.py
@@ -1,25 +1,8 @@
-import os
-from flask import Flask, send_from_directory
-from flask_cors import CORS
-from threading import Lock
+from flask import Flask
 
-async_mode = None
-app = Flask(__name__, static_folder='react-app/build') # Change your react-app name 'my-react-app'
-app.config['SECRET_KEY'] = 'secret!'
-thread_lock = Lock()
-CORS(app)
+app = Flask(__name__)
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def serve(path):
-    if path != "" and os.path.exists(app.static_folder + '/' + path):
-        return send_from_directory(app.static_folder, path)
-    else:
-        return send_from_directory(app.static_folder, 'index.html')
-
-@app.route('/test-endpoint', methods=['GET'])
-def test():
-    return "Test endpoint return"
+from views import *
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=8888, debug=True) # You can change port, host..
+    app.run(host='localhost', debug=True)
