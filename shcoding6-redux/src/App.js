871b41c09abb8bdabf95ac419269c0a6c036de2c
choijueun <songjueun815@gmail.com>
2022-06-27 17:40:05 +0900

    connect of react-redux

--- shcoding6-redux/src/App.js
+++ shcoding6-redux/src/App.js
@@ -8,7 +8,7 @@ function App() {
     return (
         <div className="App">
             <AddNumber/>
-            <DisplayNumber/>
+            <DisplayNumber unit="kg"/>
         </div>
     );
 }
--- shcoding6-redux/src/containers/DisplayNumber.js
+++ shcoding6-redux/src/containers/DisplayNumber.js
@@ -1,14 +1,19 @@
+import { connect } from "react-redux";
+import DisplayNumber from "../components/DisplayNumber";
+export default connect()(DisplayNumber) 
+/*
 import React, {useState} from 'react';
 import DisplayNumber from '../components/DisplayNumber';
 import store from '../store';
 
-export default function() {
+export default function(props) {
     const [num, setNum] = useState(store.getState().num);
     store.subscribe(function(){
         setNum(store.getState().num);
     })
 
     return (
-        <DisplayNumber num={num} unit="kg"/>
+        <DisplayNumber num={num} unit={props.unit}/>
     )
 }
+*/
