c22bcc4eaaf0a41d7bae6c739ecf954d06c429b9
choijueun <songjueun815@gmail.com>
2022-06-27 17:40:33 +0900

    return connect()(ComponentName)

--- shcoding6-redux/src/containers/DisplayNumber.js
+++ shcoding6-redux/src/containers/DisplayNumber.js
@@ -1,6 +1,9 @@
 import { connect } from "react-redux";
 import DisplayNumber from "../components/DisplayNumber";
-export default connect()(DisplayNumber) 
+export default connect()(DisplayNumber)
+
+// props를 자동으로 하위 컴포넌트에 전달
+
 /*
 import React, {useState} from 'react';
 import DisplayNumber from '../components/DisplayNumber';
