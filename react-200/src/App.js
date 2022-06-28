76a2fa54b6ad44492e9972ed9535d581649bae12
choijueun <songjueun815@gmail.com>
2022-06-28 14:29:19 +0900

    set state for props

--- react-200/src/App.js
+++ react-200/src/App.js
@@ -1,10 +1,24 @@
+import { useState } from 'react';
 import './App.css';
 import LifeCycle from './part1/R006';
 
 function App() {
+    const [number, setNumber] = useState(0);
+    function changeNumber(e) {
+        setNumber(e.target.value);
+    }
+    function printNumber(){
+        console.log(number);
+    }
+    
     return (
     <div className="App">
         <h1>REACT 200</h1>
+        <div>
+            <input type="text" value={number} onChange={changeNumber}></input>
+            <input type="button" value="PRINT" onClick={printNumber}></input>
+        </div>
+        <hr></hr>
         <LifeCycle/>
     </div>
     );
