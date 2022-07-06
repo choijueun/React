fb9b236f6c7ed2fe0fe165ba01f0bd48206454b8
choijueun <songjueun815@gmail.com>
2022-07-06 17:59:59 +0900

    ES6

--- react-200/src/part1/R009.js
+++ react-200/src/part1/R009.js
@@ -1,19 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function ES6String() {
-    const jsStr1 = '자바스크립트';
-    const jsStr2 = '입니다.\n다음줄입니다';
-    console.log(jsStr1+' 문자열'+jsStr2+'~')
-
-    const esStr1 = 'ES6';
-    const esStr2 = '입니다';
-    console.log(`${esStr1} 문자열
-    ${esStr2}~~~~`);
-
-    const longStr = 'ES6에 추가된 String 함수들';
-    console.log(longStr.startsWith('ES6'))
-    console.log(longStr.endsWith('함수들'))
-    console.log(longStr.includes('String'))
+    useEffect(()=>{
+        const jsStr1 = '자바스크립트';
+        const jsStr2 = '입니다.\n다음줄입니다';
+        console.log(jsStr1+' 문자열'+jsStr2+'~')
+    
+        const esStr1 = 'ES6';
+        const esStr2 = '입니다';
+        console.log(`${esStr1} 문자열
+        ${esStr2}~~~~`);
+    
+        const longStr = 'ES6에 추가된 String 함수들';
+        console.log(longStr.startsWith('ES6'))
+        console.log(longStr.endsWith('함수들'))
+        console.log(longStr.includes('String'))
+    },[])
 
     return (
     <>
