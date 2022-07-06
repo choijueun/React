6d93ac1451e801bd2405e41682eb5924b669fcdf
choijueun <songjueun815@gmail.com>
2022-07-06 19:02:48 +0900

    map

--- react-200/src/part1/R015.js
+++ react-200/src/part1/R015.js
@@ -1,6 +1,23 @@
 import React from 'react'
 
 function Map() {
+    const arr = [8, 2, 3, 6];
+
+    const mapArr1 = arr.map(x => x);
+    console.log(mapArr1);
+    
+    const mapArr2 = arr.map(x => x+1);
+    console.log(mapArr2);
+
+    const objArr = [{key: 'react', value: 200},
+                    {key: '리액트', value: 'Two Hunderds'}]
+
+    objArr.map((obj, index)=>{
+        console.log(obj)
+        console.log(obj.key, obj.value)
+        console.log(index)
+    })
+
     return (
         <div>Map</div>
     )
