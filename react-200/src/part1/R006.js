a4f829597a4f43130393fce7c29ecb8f7eceb442
choijueun <songjueun815@gmail.com>
2022-07-06 17:52:48 +0900

    LifeCycle

--- react-200/src/part1/R006.js
+++ react-200/src/part1/R006.js
@@ -3,12 +3,22 @@ import React, { useEffect } from 'react'
 function LifeCycle(props) {
     useEffect(()=>{
         console.log('useEffect 1')
+        return ()=>{
+            console.log('useEffect 1 return')
+        }
     })
     useEffect(()=>{
         console.log('useEffect 2')
+
+        return ()=>{
+            console.log('useEffect 2 return')
+        }
     }, [])
     useEffect(()=>{
         console.log('useEffect 3')
+        return ()=>{
+            console.log('useEffect 3 return')
+        }
     }, [props])
 
 
