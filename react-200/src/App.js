d74d78e86a768f4aa8a3894e813a603b094a54c2
choijueun <songjueun815@gmail.com>
2022-07-06 17:41:48 +0900

    R006

--- react-200/src/App.js
+++ react-200/src/App.js
@@ -19,7 +19,7 @@ function App() {
             <input type="button" value="PRINT" onClick={printNumber}></input>
         </div>
         <hr></hr>
-        <LifeCycle/>
+        <LifeCycle props_value={number}/>
     </div>
     );
 }
--- react-200/src/part1/R006.js
+++ react-200/src/part1/R006.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-function LifeCycle() {
-  return (
+function LifeCycle(props) {
+    useEffect(()=>{
+        console.log('useEffect 1')
+    })
+    useEffect(()=>{
+        console.log('useEffect 2')
+    }, [])
+    useEffect(()=>{
+        console.log('useEffect 3')
+    }, [props])
+
+
+    return (
     <div>R006</div>
-  )
+    )
 }
 
 export default LifeCycle
