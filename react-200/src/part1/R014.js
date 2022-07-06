2b2ac5a5c93e2a64f1d343764891dfafe3602fc8
choijueun <songjueun815@gmail.com>
2022-07-06 18:47:56 +0900

    forEach

--- react-200/src/part1/R014.js
+++ react-200/src/part1/R014.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
 function ForEach() {
-  return (
-    <div>ForEach</div>
-  )
+    const arr = [1, 3, 6, 7, 9];
+    const newArr1 = [];
+    const newArr2 = [];
+
+    /* for */
+    for(let i=0; i<arr.length; i++) {
+        newArr1.push(arr[i]);
+    }
+    /* forEach */
+    arr.forEach((el)=>{
+        newArr2.push(el);
+    })
+
+    console.log('newArr1', newArr1)
+    console.log('newArr2', newArr2)
+
+    return (
+        <div>ForEach</div>
+    )
 }
 
 export default ForEach
