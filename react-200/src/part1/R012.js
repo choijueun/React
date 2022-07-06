72e6db2aa5e02fbc5b374f143ecd891f2a38d3a8
choijueun <songjueun815@gmail.com>
2022-07-06 18:32:48 +0900

    ES6 Class

--- react-200/src/part1/R012.js
+++ react-200/src/part1/R012.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function ClassPrototype() {
-  return (
+    const [num, setNum] = useState(0);
+    
+    useEffect(()=>{
+
+        /* ES5 Prototype */
+        var exFn = (function(){
+            function exCount(number) {
+                setNum(number);
+            }
+            exCount.prototype.showNum = function(){
+                console.log('1. ES5 prototype: ',num)
+            }
+            return exCount
+        }())
+        const cnt1 = new exFn(200);
+        cnt1.showNum();
+
+
+        /* ES6 Class */
+        class exClass {
+            constructor(number2) {
+                setNum(number2);
+            }
+            showNum() {
+                console.log(`2. ES6 Class: ${num}`);
+            }
+        }
+        const cnt2 = new exClass(400);
+        cnt2.showNum();
+    })
+
+
+    return (
     <div>ClassPrototype</div>
-  )
+    )
 }
 
 export default ClassPrototype
