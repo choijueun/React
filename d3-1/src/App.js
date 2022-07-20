7198747dd24aa069a3d5bea168c50da98c8a2b59
choijueun <songjueun815@gmail.com>
2022-07-20 18:25:47 +0900

    Nav

--- d3-1/src/App.js
+++ d3-1/src/App.js
@@ -1,14 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css';
+
 import Sample from './components/Sample';
+import CurvedLine from './components/CurvedLine';
+
+
+function Nav(props){
+    const nav     = ['Sample(Circle)', 'CurvedLine']
+    const content = [<Sample/>, <CurvedLine/>]
+
+    let li = []
+    nav.forEach((n, i)=>{
+        li = li.concat(<li key={i} id={i} onClick={changeTab}>{n}</li>)
+    })
+    function changeTab(e){
+        const idx = e.target.id
+        props.setBody(content[idx])
+    }
+
+    return <ul>
+        {li}
+    </ul>
+}
 
 
 function App() {
+    const [ body, setBody ] = useState(<Sample/>)
+
     return <>
         <a href='https://youtu.be/Y-ThTzB-Zjk' target={"_blank"}>
             <button>► Using React (Hooks) with D3</button>
         </a>
-        <Sample/>
+        <Nav setBody={setBody}/>
+        <hr/>
+        {body}
     </>
 }
 
