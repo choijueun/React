e25cf37659713fee8b0b618537c9273fab809ca0
choijueun <songjueun815@gmail.com>
2022-07-20 18:58:53 +0900

    주석

--- d3-1/src/components/Sample.js
+++ d3-1/src/components/Sample.js
@@ -10,18 +10,19 @@ function Sample() {
         const svg = select(svgRef.current); // selection 객체
 
         svg.selectAll("circle")    // circle tag 전부 찾기
-            .data(data)             // data binding
+            .data(data)            // data binding
             // create update delete 관리
-            .join(      // .join('circle) //간단
+            .join( // .join('circle) 동일
                 (enter) => enter.append("circle"),           // circle 없다면 생성
                 (update) => update.attr("class", "updated"), // circle 있다면 업데이트(updated class 추가)
                 (exit) => exit.remove()                      // 남는 circle 지우기 (?)
             )
             // [속성] 
-            .attr("r", (value) => value)        // r    반지름 
-            .attr("cx", (value) => value * 2)   // cx   중심점 x좌표 
-            .attr("cy", (value) => value * 2)   // cy   중심점 y좌표
-            .attr("stroke", "blue");
+            .attr("r", (value) => value)        // r       반지름 
+            .attr("cx", (value) => value * 2)   // cx      중심점 x좌표 
+            .attr("cy", (value) => value * 2)   // cy      중심점 y좌표
+            .attr('fill', 'yellow')
+            .attr("stroke", "blue");            // storke  선 색상
     }, [data]);
 
     return (
