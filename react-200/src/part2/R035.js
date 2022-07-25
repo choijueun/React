0b4aef2f7863205e4d2c3cf0ee66d9f749cb4abb
choijueun <songjueun815@gmail.com>
2022-07-25 16:41:47 +0900

    reactstrap badge & button

---
+++ react-200/src/part2/R035.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Badge, Button } from 'reactstrap'
+
+function ReactstrapBadges() {
+    return <div style={{margin: 10}}>
+        <h2>reactstrap badge</h2>
+        <p>
+            PRODUCT <Badge color='danger'>50%</Badge> 
+        </p>
+        <p>
+            <Button color='info' outline>
+                CART(outline) <Badge color='info'>3</Badge>
+            </Button>
+        </p>
+        <p>
+            <Badge href="/hi" color='warning' pill>Link & pill</Badge>
+        </p>
+    </div>
+}
+
+export default ReactstrapBadges
