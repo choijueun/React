b18d5898753110e902ce04912897dc8b1ef7eeae
choijueun <songjueun815@gmail.com>
2022-07-25 15:13:23 +0900

    comment

--- react-200/src/part1/R015.js
+++ react-200/src/part1/R015.js
@@ -4,20 +4,24 @@ function Map() {
     const arr = [8, 2, 3, 6];
 
     const mapArr1 = arr.map(x => x);
+    console.log('# mapArr1');
     console.log(mapArr1);
     
     const mapArr2 = arr.map(x => x+1);
+    console.log('# mapArr2');
     console.log(mapArr2);
 
     const objArr = [{key: 'react', value: 200},
                     {key: '리액트', value: 'Two Hunderds'}]
 
     const objArr2 = objArr.map((obj, index)=>{
+        console.log(`# objArr - ${index}`)
         console.log(obj)
         console.log(obj.key, obj.value)
         console.log(index)
         return {value: obj.value, idx: index}
     })
+    console.log('# objArr2')
     console.log(objArr2)
 
     return (
