c2ec07995d26b9e466687b8daaf44aa678d0c3d1
choijueun <songjueun815@gmail.com>
2022-07-25 18:31:56 +0900

    reactstrap card

--- react-200/src/part2/reactstrap/R040.js
+++ react-200/src/part2/reactstrap/R040.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import {
+    Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button
+} from 'reactstrap'
 
 function ReactstrapCard() {
     return (
-    <div>
+    <div style={{width: 500}}>
         <h2>ReactstrapCard</h2>
+        <Card>
+            <CardImg top src="https://cdn2.thecatapi.com/images/MTY1MjExMA.jpg" alt='cat raising up its arms'/>
+            <CardBody>
+                <CardTitle>TITLE</CardTitle>
+                <CardSubtitle>subtitle</CardSubtitle>
+                <CardText>card body - text</CardText>
+                <CardSubtitle>subtitle</CardSubtitle>
+                <CardText>card body - text</CardText>
+                <Button color='primary' outline>btn</Button>
+            </CardBody>
+        </Card>
     </div>
     )
 }
